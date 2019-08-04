59ad69c2ab386758b29fac15e34317e888d13298
lynx-r <aleksey.p10n@gmail.com>
2019-08-04 10:37:51 +0300

    swUpdate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwPush, SwUpdate } from '@angular/service-worker';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-    title = 'angular-webrtc';
+export class AppComponent implements OnInit {
+
+    constructor(
+        private swUpdate: SwUpdate,
+        private swPush: SwPush,
+    ) {
+    }
+
+    ngOnInit(): void {
+        if (this.swUpdate.isEnabled) {
+            this.swUpdate.available.subscribe(() => {
+                window.location.reload();
+            });
+        }
+    }
+
 }
